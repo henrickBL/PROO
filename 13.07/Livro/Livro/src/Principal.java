9553d8048f12028cef8a43b2d1005c3a6623761a
henrickBL <jhbl1@aluno.ifal.edu.br>
2022-07-17 01:09:54 +0000

    Código teste

---
+++ 13.07/Livro/Livro/src/Principal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args){
+    Scanner inserir = new Scanner(System.in);
+    String cor;
+    int x;
+
+        System.out.println("Qual a cor da camisa?");
+        cor = inserir.next();
+    
+
+    if (cor.equalsIgnoreCase("branca")){
+        System.out.println("funcionou");
+    }
+    }
+}
