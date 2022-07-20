d637ddac509dc045940023993d3ea2979d450529
henrickBL <jhbl1@aluno.ifal.edu.br>
2022-07-20 12:15:55 +0000

    atividade

--- 20.07/exercicio/src/Elevador.java
+++ 20.07/exercicio/src/Elevador.java
@@ -4,7 +4,22 @@ public class Elevador {
     String nome;
     int andar_atual; //térreo == 0
     int total_andares; //desconsiderando o térreo
-    
+    int capacidade;
 
+    Elevador(){
+        capacidade = 6;
+        andar_atual = 0;
+        total_andares = 5;
+    }
+
+    Elevador(capacidade, total_andares){
+        capacidade = inserir.nextInt();
+        total_andares = inserir.nextInt();
+
+    }
+
+    void Entrar(){
+        
+    }
     inserir.close();
 }
