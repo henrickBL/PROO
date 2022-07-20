7ada5cda17c2c8d13a3cbb1170bda5b6a21d059c
henrickBL <jhbl1@aluno.ifal.edu.br>
2022-07-20 12:12:34 +0000

    atividade

--- 20.07/exercicio/src/Elevador.java
+++ 20.07/exercicio/src/Elevador.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-public class Principal {
+public class Elevador {
     Scanner inserir = new Scanner(System.in);
+    String nome;
+    int andar_atual; //térreo == 0
+    int total_andares; //desconsiderando o térreo
     
+
+    inserir.close();
 }
--- 20.07/exercicio/src/Principal.java
+++ 20.07/exercicio/src/Principal.java
@@ -3,5 +3,7 @@ public class Principal {
     public static void main(String[] args) {
         Scanner inserir = new Scanner(System.in);
         
+        
+        inserir.close();
     }
 }
