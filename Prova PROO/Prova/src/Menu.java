2a39282e0cab0d3dabe5c50c00eabb8f25303145
henrickBL <jhbl1@aluno.ifal.edu.br>
2022-08-13 20:27:55 -0300

    código pronto

--- Prova PROO/Prova/src/Menu.java
+++ Prova PROO/Prova/src/Menu.java
@@ -269,7 +269,7 @@ public class Menu {
         equipe[num].treinador.setTreinador();
     }
 
-    // 7 - Mudar treinador
+    // 7 - Exibir equipe
     public void exibirEquipe() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
