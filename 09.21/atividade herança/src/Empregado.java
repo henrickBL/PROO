71da169a2f9be14e8ae178863d48eca355dd3ae9
henrickBL <jhbl1@aluno.ifal.edu.br>
2022-09-21 14:32:09 +0000

    atividade herança

--- 09.21/atividade herança/src/Empregado.java
+++ 09.21/atividade herança/src/Empregado.java
@@ -1,3 +1,10 @@
-public class Empregado {
-    
+public class Empregado extends Pessoa {
+    private String nome;
+    private String cpf;
+    private double salario;
+    private String cargo;
+
+    Empregado(String cargo, double salario, String cpf, String nome) {
+        super(nome, cpf);
+    }
 }
